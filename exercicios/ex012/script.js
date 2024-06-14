37359737af790a52b7479741bb395f90c68566ad
pablocastro444 <163168579+pablocastro444@users.noreply.github.com>
2024-06-14 20:25:27 -0300

    js

--- exercicios/ex012/script.js
+++ exercicios/ex012/script.js
@@ -11,10 +11,35 @@ function verificar() {
         var gênero = ''
         if (fsex[0].checked) {
             gênero = 'Homem'
+            if (idade >=0 && idade < 10) {
+                //CRIANÇA
+                img.setAttribute('src', 'foto-bebe-m.png')
+            } else if (idade < 21) {
+                //JOVEM
+                img.setAttribute('src', 'foto-jovem-m.png')
+            } else if (idade < 50) {
+                //ADULTO
+                img.setAttribute('src','foto-adulto-m.png')
+            } else {
+                //IDOSO
+                img.setAttribute('')
+            }
+                
+
         } else if (fsex[1].checked) {
             gênero = 'Mulher'
+            if (idade >=0 && idade < 10) {
+                //CRIANÇA
+            } else if (idade < 21) {
+                //JOVEM
+            } else if (idade < 50) {
+                //ADULTO
+            } else {
+                //IDOSO
+            }
         }
         res.style.textAlign = 'center'
         res.innerHTML = `Detectamos ${gênero} com ${idade} anos`
+        res.appendChild(img)
     }
 }
